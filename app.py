6aec50020f6a965e39a434faf83e5264f88f059b
Olga Skargovskaya <olgascar84@icloud.com>
2023-07-11 19:59:56 +0300

    Create project

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlte:///shop.db'
 
 @app.route('/')
 def index():
@@ -10,9 +12,9 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/create')
-def create():
-    return render_template('create.html')
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(Debug=True)
